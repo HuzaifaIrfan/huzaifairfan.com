b38be831a7a452c6d53dc0fd79b6225d53d6c821
HuzaifaIrfan <huzaifairfan2001@gmail.com>
2025-07-12 12:29:14 +0500

    rm old font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 
 import { Comic_Neue } from 'next/font/google';
 
-
-
-
 const comic = Comic_Neue({ subsets: ['latin'], weight: '400' });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Huzaifa Irfan",
